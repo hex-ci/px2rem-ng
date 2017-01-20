69f10426c65cda370f400b8bb1494fea69d09929
Hex <hex@codeigniter.org.cn>
2017-01-20 14:57:02 +0800

    改进测试用例

--- spec/px2rem-plus-spec.js
+++ spec/px2rem-plus-spec.js
@@ -1,73 +1,36 @@
 'use babel';
 
-import Pxrem from '../lib/px2rem';
+import Px2rem from '../lib/index';
 
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
-describe('Px2rem', () => {
+describe('Px2rem Plus', () => {
   let workspaceElement, activationPromise;
 
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
     activationPromise = atom.packages.activatePackage('px2rem-plus');
+    waitsForPromise(() => {
+      return activationPromise;
+    });
   });
 
-  describe('when the pxrem:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.px2rem')).not.toExist();
+  it('converts', () => {
+    let editor = atom.workspace.getActiveTextEditor();
+    editor.insertText('6px');
+    editor.selectAll();
+    let changeHandler = jasmine.createSpy('changeHandler');
+    editor.onDidChange(changeHandler);
+    atom.commands.dispatch(workspaceElement, 'px2rem-plus:convert');
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'px2rem-plus:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.px2rem')).toExist();
-
-        let pxremElement = workspaceElement.querySelector('.px2rem');
-        expect(pxremElement).toExist();
-
-        let pxremPanel = atom.workspace.panelForItem(pxremElement);
-        expect(pxremPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'px2rem-plus:toggle');
-        expect(pxremPanel.isVisible()).toBe(false);
-      });
+    waitsForPromise(() => {
+      return activationPromise;
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
+    waitsFor(() => {
+      return changeHandler.callCount > 0;
+    });
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.px2rem')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'px2rem-plus:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let pxremElement = workspaceElement.querySelector('.px2rem');
-        expect(pxremElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'px2rem-plus:toggle');
-        expect(pxremElement).not.toBeVisible();
-      });
+    return runs(() => {
+      return expect(editor.getText()).toEqual('0.08rem');
     });
   });
 });
